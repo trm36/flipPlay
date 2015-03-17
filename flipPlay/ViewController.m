648a4b1df99e233182b509364f35e57479e1a592
Taylor Mott <trm36@humboldt.edu>
2015-03-17 15:39:08 -0600

    Initial Commit

---
+++ flipPlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  flipPlay
+//
+//  Created by Taylor Mott on 17.3.2015.
+//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
